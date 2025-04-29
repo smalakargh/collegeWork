80235dfb038c50a46423fb008e225976eaac9e50
smalakargh <supriyomic@gmail.com>
2025-04-29 10:17:12 +0530

    matrix Uploaded

---
+++ matrix/checkSame.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, isEqual = 1;
+
+    printf("Enter the number of rows and columns for the matrices: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix1[rows][cols], matrix2[rows][cols];
+
+    printf("Enter elements of the first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Enter element at [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Enter element at [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            if (matrix1[i][j] != matrix2[i][j]) {
+                isEqual = 0; 
+                break;
+            }
+        }
+        if (isEqual != 0) {break};
+    }
+
+
+    if (isEqual) {
+        printf("The two matrices are equal.\n");
+    } else {
+        printf("The two matrices are not equal.\n");
+    }
+
+    return 0;
+}
